555245e9421e089010914585536d08bf716408a9
Patrick Shinn <patrick_shinn@yahoo.com>
2016-09-01 17:08:07 -0400

    UPDATE

--- M.A.T.T.ino
+++ M.A.T.T.ino
@@ -1,7 +1,7 @@
 /*
  * Author: Patrick Shinn
- * Version: 1.5
- * Date: 3/05/16
+ * Version: 2.0
+ * Date: 6/4/16
  * Vehicle: M.A.T.T
  * -------------------------------------------------------------
  * # Mechanical Autonomous Treaded Tank
@@ -20,37 +20,65 @@ MeUltrasonicSensor ultraSensor(PORT_4); // Distance Sensor that rotates
 MeUltrasonicSensor ultraSensor1(PORT_3); // Distance Sensor that is stationary
 MeDCMotor motor1(M1); // DC motor 1
 MeDCMotor motor2(M2); // DC motor 2
-
+Servo servo; // creates a servo object.
+MePort port(PORT_6); // creates an MePort object
+int16_t servoPin = port.pin1(); // creates an attachment point for the servo.
 
 void setup() {
   // put your setup code here, to run once
-  
+  // attaches servo
+  servo.attach(servoPin);
   // Buzzer indicating M.A.T.T startup and servo test pass.
-  buzzerOn(); 
-  delay(1000);
-  buzzerOff();
-  delay(2000);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+   for (int i = 0; i < 180; i++){
+      servo.write(i);
+      delay(15);
+      // If either sesnor picks up an object within 15 cm, stop and turn left. Then check for another obstacle.
+        if((ultraSensor.distanceCm() < 15 && ultraSensor.distanceCm() > 0) || (ultraSensor1.distanceCm() < 15 && ultraSensor1.distanceCm() > 0))
+        {
+            motor1.stop();
+            motor2.stop();
+            delay(500);
+            motor1.run(-255);
+            motor2.run(-255);
+            delay(500);
+            motor1.stop();
+            motor2.stop();
+            delay(500);
+      
+        // Otherwise, full speed ahead
+        }else{
+         
+          motor1.run(-255);
+          motor2.run(255);
+        }
+      }
+    for (int i = 180; i > 0; i --){
+      servo.write(i);
+      delay(15);
+      // If either sesnor picks up an object within 15 cm, stop and turn left. Then check for another obstacle.
+        if((ultraSensor.distanceCm() < 15 && ultraSensor.distanceCm() > 0) || (ultraSensor1.distanceCm() < 15 && ultraSensor1.distanceCm() > 0))
+        {
+            motor1.stop();
+            motor2.stop();
+            delay(500);
+            motor1.run(-255);
+            motor2.run(-255);
+            delay(500);
+            motor1.stop();
+            motor2.stop();
+            delay(500);
+      
+        // Otherwise, full speed ahead
+        }else{
+         
+          motor1.run(-255);
+          motor2.run(255);
+        }
+      }
+    }
 
-  // If either sesnor picks up an object within 15 cm, stop and turn left. Then check for another obstacle.
-  if((ultraSensor.distanceCm() < 15 && ultraSensor.distanceCm() > 0) || (ultraSensor1.distanceCm() < 15 && ultraSensor1.distanceCm() > 0))
-  {
-      motor1.stop();
-      motor2.stop();
-      delay(500);
-      motor1.run(-255);
-      motor2.run(-255);
-      delay(500);
-      motor1.stop();
-      motor2.stop();
-      delay(500);
-
-  // Otherwise, full speed ahead
-  }else{
-    motor1.run(-255);
-    motor2.run(255);
-  }
-}
